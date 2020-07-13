1ac3596d586da19b16b4f8a7ebf0eea9e5eafd0e
Mauricio Cantu <ciciocantuu@gmail.com>
2020-07-13 20:01:31 -0300

    feat: added function to validate email format

---
+++ server/src/utils/validateEmail.js
@@ -0,0 +1,5 @@
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return re.test(String(email).toLowerCase());
+}
+module.exports = validateEmail;
